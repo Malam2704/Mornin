d61694dbf6ed9fcf1f22fb12a0d06b013306ae33
Mohammed Alam <mohammedal2144@gmail.com>
2022-04-13 20:24:41 -0400

    Working on Live trains API

--- src/components/Forecast/forecast.js
+++ src/components/Forecast/forecast.js
@@ -11,18 +11,40 @@ const Forecast = () => {
     const [isPageLoaded, setIsPageLoaded] = useState(false); //this helps
 
     function getForecast(e) {
+        // const https = require('https');
+        // https.get(
+        // "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm",
+        // { headers: { "x-api-key": 'SThAc9Dfpb3OJ47mVg3RP3Q9qJfAOE9F5TvwZcMs'}
+        // },
+        // (resp) => {
+        //     resp.on('data', (chunk) => {
+        //     console.log("Receiving Data");
+        //     });
+        //     resp.on('end', () => {
+        //     console.log("Finished receiving data");
+        //     });
+        // }).on("error", (err) => {
+        //     console.log("Error: " + err.message);
+        // });
+
         // weather data fetch function will go here
         const options = {
             method: 'GET',
             headers: {
-                'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
-                'X-RapidAPI-Key': 'fb978a9957msh0b56e3055da8ae7p111c46jsnfa747495b5bc'
+                // 'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
+                // 'X-RapidAPI-Key': 'fb978a9957msh0b56e3055da8ae7p111c46jsnfa747495b5bc'
+                'X-API-Key': 'SThAc9Dfpb3OJ47mVg3RP3Q9qJfAOE9F5TvwZcMs',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
         };
 
-        fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, options)
+        // let url = `https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`;
+        let url = `https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm`;
+        fetch(url, options)
             .then(response => response.json())
             .then(response => {
+                console.log(response)
                 setResponseObj(response)
             })
 
